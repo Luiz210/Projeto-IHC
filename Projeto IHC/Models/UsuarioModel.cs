204d150e95df27147ccda6b468b1a0a74b7f8572
0220482113044 <0220482113044@TC19>
2022-03-16 19:36:11 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto IHC/Models/UsuarioModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_IHC.Models
+{
+    public class UsuarioModel
+    {
+        public string nome { get; set; }
+        public int Email { get; set; }
+        public int ID { get; set; }
+        public string Senha { get; set; }
+    }
+}
